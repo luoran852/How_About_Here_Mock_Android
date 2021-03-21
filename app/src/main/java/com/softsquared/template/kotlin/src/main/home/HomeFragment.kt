ea17b3b5b381aa23b36567db2fe21ff383dff929
luoran852 <62210248+luoran852@users.noreply.github.com>
2021-03-21 23:32:46 +0900

    hotel

--- app/src/main/java/com/softsquared/template/kotlin/src/main/home/HomeFragment.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/home/HomeFragment.kt
@@ -1,19 +1,25 @@
 package com.softsquared.template.kotlin.src.main.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import androidx.fragment.app.FragmentManager
 import com.softsquared.template.kotlin.R
 import com.softsquared.template.kotlin.config.BaseFragment
 import com.softsquared.template.kotlin.databinding.FragmentHomeBinding
-import com.softsquared.template.kotlin.src.main.home.models.UserResponse
+import com.softsquared.template.kotlin.src.main.hotelArea.HotelAreaFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind,
         R.layout.fragment_home), HomeFragmentView {
 
+    val hotelAreaFragment: HotelAreaFragment = HotelAreaFragment()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.homeHotel.setOnClickListener {
+            fragmentManager?.beginTransaction()?.replace(R.id.main_frm, hotelAreaFragment)
+                    ?.commitAllowingStateLoss()
+        }
 
 
 //        binding.homeBtnTryPostHttpMethod.setOnClickListener {
---
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/hotelArea/HotelAreaFragment.kt
@@ -0,0 +1,18 @@
+package com.softsquared.template.kotlin.src.main.hotelArea
+
+import android.os.Bundle
+import android.view.View
+import com.softsquared.template.kotlin.R
+import com.softsquared.template.kotlin.config.BaseFragment
+import com.softsquared.template.kotlin.databinding.FragmentHotelAreaBinding
+
+class HotelAreaFragment : BaseFragment<FragmentHotelAreaBinding>(FragmentHotelAreaBinding::bind,
+        R.layout.fragment_hotel_area) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
